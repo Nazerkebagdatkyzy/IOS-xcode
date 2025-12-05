2eda0187780d2cc93c1d5bc3be3b32ac58f3c494
Nazerke <bagnazerke581@gmail.com>
2025-12-05 10:35:16 +0500

    Initial Commit

---
+++ AttendanceTracker/AttendanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttendanceTrackerApp.swift
+//  AttendanceTracker
+//
+//  Created by Nazerke Bagdatkyzy on 05.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct AttendanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
